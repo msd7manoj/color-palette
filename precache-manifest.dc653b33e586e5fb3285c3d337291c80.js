9044d71b53a3fcd4d263bb9ad490ec1f8e8b7f9d
Manojkumar Shanmugam <mshanmugam@AD.MSYSTECHNOLOGIES.COM>
2020-06-28 10:37:15 +0530

    Updates

---
+++ precache-manifest.dc653b33e586e5fb3285c3d337291c80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8625a72bf076d07a579cd64bd5e29c4e",
+    "url": "/github.io/color-palette/index.html"
+  },
+  {
+    "revision": "d5df8463989f7c7eaaa0",
+    "url": "/github.io/color-palette/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "71e16ebf5dafc6d43204",
+    "url": "/github.io/color-palette/static/css/main.4d630dbb.chunk.css"
+  },
+  {
+    "revision": "d5df8463989f7c7eaaa0",
+    "url": "/github.io/color-palette/static/js/2.f93cde8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/color-palette/static/js/2.f93cde8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e16ebf5dafc6d43204",
+    "url": "/github.io/color-palette/static/js/main.0d31295c.chunk.js"
+  },
+  {
+    "revision": "63b9a1951c6b7df11350",
+    "url": "/github.io/color-palette/static/js/runtime-main.99c00969.js"
+  }
+]);
