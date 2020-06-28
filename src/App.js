ac5fc5cc5542dfbf25607cb3889e70a83f46b221
Manojkumar Shanmugam <mshanmugam@AD.MSYSTECHNOLOGIES.COM>
2020-06-28 18:20:58 +0530

    Developed design

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { PALETTE_COLORS } from './constants/palettesColor';
-
+import Drawer from 'rc-drawer'
+import "rc-drawer/assets/index.css";
 
 function App() {
 
   const [state, setstate] = useState([])
+  const [drawerOpen, setDrawerOpen] = useState(false)
 
-  const changeView = (type) => (e) => {
+
+
+
+  const toggleDrawer = (e) => {
     e.preventDefault()
+    setDrawerOpen(true)
   }
+
+
   useEffect(() => {
     setstate(() => {
       return PALETTE_COLORS.map( (el, ind) => {
@@ -112,7 +120,7 @@ function App() {
                           <a title="Open Palette" className="iconLink" href="/">
                             <i class="fas fa-palette"></i>
                           </a>
-                          <a title="View Palette" className="iconLink" href="/">
+                          <a title="View Palette" onClick={toggleDrawer} className="iconLink" href="/">
                             <i class="fas fa-swatchbook"></i>
                           </a>
                         </div>
@@ -124,7 +132,7 @@ function App() {
                           {el.palettes.map( palette => {
                             return (
                               <li key={palette} style={{background: `#${palette}`}}>
-                                {/* <span className="colorToolTip">{palette}</span> */}
+                                <span className="colorToolTip">{palette}</span>
                               </li>
                             )
                           })} 
@@ -160,7 +168,198 @@ function App() {
             })
           }
         </Row>
-      </div>
+     </div>
+
+
+
+
+
+
+
+
+
+
+    <Drawer 
+    showMask={false}
+    handler={false} 
+    parent={null} 
+    level={null} 
+    placement="right" 
+    handleChild={false}
+    open={drawerOpen}
+    // open={false}
+    width="380px">
+        <div className="paletteDrawerWrp">
+          <div className="dFlex justify-between paletteDrawerHeader">
+            <h3>View Palette</h3>
+            <a href="/">
+              <i class="fas fa-arrow-right"></i>
+            </a>
+          </div>
+
+          <div className="paletteTabWrp">
+            <ul className="drawerPaletteTab dFlex">
+              <li className="active" style={{background: 'red'}}>
+                <span><i class="lni lni-checkmark"></i></span>
+              </li>
+              <li style={{background: 'orange'}}>
+                <span><i class="lni lni-checkmark"></i></span>
+              </li>
+              <li style={{background: 'yellow'}}>
+                <span><i class="lni lni-checkmark"></i></span>
+              </li>
+              <li style={{background: 'green'}}>
+                <span><i class="lni lni-checkmark"></i></span>
+              </li>
+              <li style={{background: 'teal'}}>
+                <span><i class="lni lni-checkmark"></i></span>
+              </li>
+            </ul>
+          </div>
+
+          <div className="paletteTabContWrp">
+            <div>
+              <ul className="colorCodeLists">
+                <li className="dFlex justify-between">
+                  <div className="colorCode">
+                    <h4>HEX</h4>
+                    <h3>
+                      123445
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                    
+                  </div>
+
+                  <div className="colorCode">
+                    <h4>HEX CODE</h4>
+                    <h3>
+                      #123445
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                  </div>
+                </li>
+
+                <li className="dFlex justify-between">
+                  <div className="colorCode">
+                    <h4>RGB</h4>
+                    <h3>
+                      12, 45, 89
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                  </div>
+
+                  <div className="colorCode code_rt">
+                    <h4>RGB CODE</h4>
+                    <h3>
+                      RGB(12, 45, 89)
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                  </div>
+                </li>
+
+                <li className="dFlex justify-between">
+                  <div className="colorCode">
+                    <h4>HSB</h4>
+                    <h3>
+                      12, 45, 89
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                  </div>
+
+                  <div className="colorCode code_rt">
+                    <h4>HSB CODE</h4>
+                    <h3>
+                    HSB(12, 45, 89)
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                  </div>
+                </li>
+
+
+                <li className="dFlex justify-between">
+                  <div className="colorCode">
+                    <h4>HSL</h4>
+                    <h3>
+                      12, 45, 89
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                  </div>
+
+                  <div className="colorCode code_rt">
+                    <h4>HSL CODE</h4>
+                    <h3>
+                      HSL(12, 45, 89)
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                  </div>
+                </li>
+
+                <li className="dFlex justify-between">
+                  <div className="colorCode">
+                    <h4>CMYK</h4>
+                    <h3>
+                      12, 45, 89, 34
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                  </div>
+
+                  <div className="colorCode code_rt">
+                    <h4>CMYK CODE</h4>
+                    <h3>
+                      CMYK(12, 45, 89, 34)
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                  </div>
+                </li>
+
+                <li className="dFlex justify-between">
+                  <div className="colorCode">
+                    <h4>Name</h4>
+                    <h3>
+                      Red
+                      <a href="/"><i class="far fa-copy"></i></a>
+                    </h3>
+                  </div>
+                </li>
+
+
+              </ul>
+            </div>
+          </div>
+
+          <div className="paletteNav dFlex justify-between">
+            <div className="dFlex">
+              {/* <a title="Previous Palette" className="iconLink" href="/">
+                <i class="lni lni-chevron-left"></i>
+              </a> */}
+
+              <a title="Export Palette" className="iconLink" href="/">
+                <i class="fas fa-file-export"></i>
+              </a>
+              <a title="Copy All" className="iconLink" href="/">
+                <i class="far fa-copy"></i>
+              </a>
+              <a title="Open Palette" className="iconLink" href="/">
+                <i class="fas fa-palette"></i>
+              </a>
+            </div>
+            <div className="dFlex">
+              <a title="Previous Palette" className="iconLink" href="/">
+                <i class="lni lni-chevron-left"></i>
+              </a>
+              <a title="Next Palette" className="iconLink" href="/">
+                <i class="lni lni-chevron-right"></i>
+              </a>
+            </div>
+          </div>
+
+
+        </div>
+    </Drawer>
+
+
+
+
     </div>
   );
 }
