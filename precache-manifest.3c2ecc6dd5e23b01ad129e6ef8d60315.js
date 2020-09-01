8c3ec7f4e3a4215e6f3552bb40416a0be5f05c51
Manojkumar Shanmugam <mshanmugam@AD.MSYSTECHNOLOGIES.COM>
2020-09-01 21:48:40 +0530

    Updates

---
+++ precache-manifest.3c2ecc6dd5e23b01ad129e6ef8d60315.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90219ef29460648bc19dcc899a6580dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90a031f31bc51be9919b",
+    "url": "./static/css/2.b59e456e.chunk.css"
+  },
+  {
+    "revision": "49d0c51abdb8f56170c2",
+    "url": "./static/css/main.04fd3a3b.chunk.css"
+  },
+  {
+    "revision": "90a031f31bc51be9919b",
+    "url": "./static/js/2.0b0203d5.chunk.js"
+  },
+  {
+    "revision": "c16f6e7334cce5b2c87aefac50e32171",
+    "url": "./static/js/2.0b0203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49d0c51abdb8f56170c2",
+    "url": "./static/js/main.c1665b95.chunk.js"
+  },
+  {
+    "revision": "7b2f5704563c2446f2da",
+    "url": "./static/js/runtime-main.6d18571f.js"
+  },
+  {
+    "revision": "84195d9713f947c648507f673ec99da1",
+    "url": "./static/media/logo2.84195d97.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bc1b75bd4d00ae8e5cfaa3695011ab04.js"
+  "./precache-manifest.3c2ecc6dd5e23b01ad129e6ef8d60315.js"
 );
 
 self.addEventListener('message', (event) => {
